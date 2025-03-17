ca67c3b8a22c5e39cc2408c3befd325a203da7c4
Yiting-abc <2584591582@qq.com>
2025-03-17 16:15:21 +0800

    for github page

---
+++ script.js
@@ -0,0 +1,20 @@
+document.write("Hello world!");
+
+function sayHello(){
+  var response = prompt("What is you name");
+  alert("Hello " + response + ", Welcome to our game!");
+}
+
+function add(a,b){
+  return a + b;
+}
+console.log(add(20,30));
+console.log(add(150,20078));
+
+function showTopic(){
+  var x = document.getElementById("demo");
+  x.style.fontSize ="25px";
+  x.style.color ="tan";
+}
+
+  //sayHello();
